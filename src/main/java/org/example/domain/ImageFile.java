bad51c3e3d6cd1305f01d8b0c49a1d2d91fe236c
23Yong <1239824@naver.com>
2022-12-01 23:08:57 +0900

    feat: 파일 검증 로직 작성

--- src/main/java/org/example/domain/ImageFile.java
+++ src/main/java/org/example/domain/ImageFile.java
@@ -5,6 +5,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 import static org.springframework.util.StringUtils.getFilenameExtension;
@@ -18,7 +19,7 @@ public class ImageFile {
     private final String extension;
     private final byte[] imageBytes;
 
-    public ImageFile(String originalFileName, String contentType, String extension, byte[] imageBytes) {
+    private ImageFile(String originalFileName, String contentType, String extension, byte[] imageBytes) {
         this.originalFileName = originalFileName;
         this.contentType = contentType;
         this.extension = extension;
@@ -27,9 +28,39 @@ public class ImageFile {
 
     public static ImageFile from(final MultipartFile multipartFile) {
         try {
+            validateNullFile(multipartFile);
+            validateEmptyFile(multipartFile);
+            validateExtensionFile(multipartFile);
+
             return new ImageFile(multipartFile.getOriginalFilename(), multipartFile.getContentType(), getFilenameExtension(multipartFile.getOriginalFilename()), multipartFile.getBytes());
         } catch (IOException e) {
             throw new IllegalArgumentException("Invalidate file" + multipartFile);
+        }
+    }
+
+    private static void validateNullFile(final MultipartFile multipartFile) {
+        if (Objects.isNull(multipartFile)) {
+            throw new IllegalArgumentException("File is null");
+        }
+    }
+
+    private static void validateEmptyFile(final MultipartFile multipartFile) {
+        if (multipartFile.isEmpty()) {
+            throw new IllegalArgumentException("File is empty");
+        }
+    }
+
+    private static void validateExtensionFile(final MultipartFile multipartFile) {
+        String extension = getFilenameExtension(multipartFile.getOriginalFilename());
+
+        if (!VALIDATE_IMAGE_FILE_EXTENSION.matcher(extension).matches()) {
+            throw new IllegalArgumentException("Invalidate extension");
+        }
+    }
+
+    private static void validateEmptyFileName(final MultipartFile multipartFile) {
+        if (multipartFile.getOriginalFilename().isEmpty()) {
+            throw new IllegalArgumentException("Invalidate file name");
         }
     }
 
